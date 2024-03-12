e6f5804091f9341b1c0fab16e3956172f6cdaf44
amirsabahi <ah.sabahi@outlook.com>
2024-03-12 21:16:50 +0330

    Add task schedule

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\Todo;
+use Carbon\Carbon;
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::call(function () {
+    $twoDaysAgo = Carbon::now()->subDays(2)->startOfDay();
+    $tasks = Todo::whereDate('created_at', $twoDaysAgo)->get();
+    $tasks->update(['completed' => true]);
+})->daily();
