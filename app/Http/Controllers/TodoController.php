c22105a0de252ecabf40a598bc2ca871765d3c4d
amirsabahi <ah.sabahi@outlook.com>
2024-03-12 20:38:22 +0330

    Add tasks and update the complete status of the tasks

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function create(Request $request)
+    {
+        Validator::make($request->all(), [
+            'title' => 'required',
+            'description' => 'required',
+        ])->validate();
+
+        $todo = Todo::create([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+
+        return response()->json(['message' => 'Todo created successfully', 'todo' => $todo], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $todo = Todo::findOrFail($id);
+        return response()->json(['todo' => $todo], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        throw new \Exception('Method not implemented');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->update($request->all());
+        return response()->json(['message' => 'Todo updated successfully', 'todo' => $todo], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Todo::destroy($id);
+        return response()->json(['message' => 'Todo deleted successfully'], 200);
+    }
+
+    public function toggleIsCompleted(int $id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->update(['completed' => !$todo->completed]);
+        return response()->json(['message' => 'Todo updated successfully', 'todo' => $todo], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use \App\Http\Controllers\TodoController;
+use \App\Models\Todo;
+use \App\Http\Resources\TodoResource;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
@@ -10,5 +13,10 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::get('/todos', function () {
+        return new TodoResource(Todo::all());
+    });
+    Route::post('todo/toggle/{id}', [TodoController::class, 'toggleIsCompleted']);
+    Route::post('todo', [TodoController::class, 'create']);
 });
 
