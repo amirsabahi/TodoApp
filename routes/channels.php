4b1a4e1d3ce536ede5aa7d014f47c2100124bfd4
amirsabahi <ah.sabahi@outlook.com>
2024-03-13 15:58:53 +0330

    Commented out unneeded code

--- routes/channels.php
+++ routes/channels.php
@@ -2,12 +2,10 @@
 
 use Illuminate\Support\Facades\Broadcast;
 use App\Models\Todo;
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
+// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });
 
 Broadcast::channel("todo.{id}", function (Todo $todo, $id) {
-
     return $todo->id == (int)$id;
-
 });
